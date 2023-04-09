88f3d35cf28c55e0753845a6dff449cb36ecaf82
rahmanaga <78140740+rahmanaga@users.noreply.github.com>
2023-04-09 16:47:52 +0400

    Create the constructor func and a function for adding book to library

---
+++ app.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(author, title, pages, read = false) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
